f04aedcfa02a4b2ca27480485acdd2bfeb4a2f6a
Andy <48938478+aott33@users.noreply.github.com>
2022-05-25 10:31:40 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+function App() {
+  /*
+  Define state variables for 
+  contacts and appointments 
+  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  /*
+  Implement functions to add data to
+  contacts and appointments
+  */
+  const addContact = (item) => {
+    setContacts(prev => {return [...prev, item]});
+  }
+
+  const addAppointment = (item) => {
+    setAppointments(prev => {return [...prev, item]});
+  }
+
+  return (
+    <>
+      <nav>
+        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+          Appointments
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={ROUTES.CONTACTS} />
+          </Route>
+          <Route path={ROUTES.CONTACTS}>
+            <ContactsPage contacts = {contacts} addContact = {addContact}/>
+          </Route>
+          <Route path={ROUTES.APPOINTMENTS}>
+            <AppointmentsPage appointments = {appointments} addAppointment = {addAppointment}/>
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/containers/contactsPage/ContactsPage.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from "react";
+import {ContactForm} from '../../components/contactForm/ContactForm';
+import {TileList} from '../../components/tileList/TileList';
+
+export const ContactsPage = () => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+  const contacts = this.props.contacts;
+  const addContact = this.props.addContact;
+
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
+
+  useEffect(() => {
+    const duplicate = contacts.some(e => e.name === name);
+    if (duplicate) {
+      setDuplicate(true);
+    }
+   }, [contacts, name]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+    if (!duplicate) {
+      addContact({name: name, phone: phone, email: email});
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
+  };
+
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
+  
+
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2>
+        <ContactForm name={name} setName={setName} phone={phone} setPhone={setPhone} email={email} setEmail={setEmail} handleSubmit={handleSubmit} />
+      </section>
+      <hr />
+      <section>
+        <h2>Contacts</h2>
+        <TileList contacts={contacts}/>
+      </section>
+    </div>
+  );
+};
